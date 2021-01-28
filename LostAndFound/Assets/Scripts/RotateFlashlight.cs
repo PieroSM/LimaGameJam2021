d037c9cf91d98b82b9e3a1ead8a40e1ea2f14c1a
MrPanda <36211492+PieroSM@users.noreply.github.com>
2021-01-28 18:04:42 -0500

    Add 2D light effects

---
+++ LostAndFound/Assets/Scripts/RotateFlashlight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateFlashlight : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 1f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ LostAndFound/Assets/Scripts/SpotlightController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Transform from Screen coordinates (pixels) to World coordinates (units)
+        Vector2 screenPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+        transform.position = worldPos;
+
+    }
+
+}
