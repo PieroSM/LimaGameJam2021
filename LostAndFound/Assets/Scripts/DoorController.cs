16d00a9e12856efaf88a22925cd57dacb91f55f1
Vazkuz <d.madalengoitia@pucp.edu.pe>
2021-01-30 00:22:15 -0500

    Mecánica de llaves y puerta
    Agregué mecánica básica de cogido de llaves y apertura de puertas.

---
+++ LostAndFound/Assets/Scripts/DoorController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    bool triggerCollision = false;
+    [SerializeField] ItemDisplay itemRequired;
+    [SerializeField] int numberOfItemsToOpen;
+
+    
+    void Update()
+    {
+        OpenDoor();
+    }
+
+    private void OpenDoor()
+    {
+        if (triggerCollision)
+        {
+            if (Input.GetKeyDown("e") && itemRequired.GetNumberOfItems() >= numberOfItemsToOpen)
+            {
+                Debug.Log("Abrir puerta");
+                itemRequired.TakeItemsFromInventory(numberOfItemsToOpen);
+            }
+            else if (Input.GetKeyDown("e"))
+            {
+                Debug.Log("Faltan llaves");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<Player>())
+        {
+            triggerCollision = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        triggerCollision = false;
+    }
+
+}
---
+++ LostAndFound/Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemDisplay : MonoBehaviour
+{
+    int numberOfItems;
+    [SerializeField] TextMeshProUGUI itemText;
+
+    void Start() 
+    {
+        itemText.text = numberOfItems.ToString();
+    }
+    
+    public void AddItemsToInventory()
+    {
+        numberOfItems++;
+        itemText.text = numberOfItems.ToString();
+    }
+
+    public void TakeItemsFromInventory(int itemsTaken)
+    {
+        numberOfItems -= itemsTaken;
+        itemText.text = numberOfItems.ToString();
+    }
+
+    public int GetNumberOfItems()
+    {
+        return numberOfItems;
+    }
+
+}
